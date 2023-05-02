d3376b64c5a4f25ab5f4ce5944a828e8daead300
JustinLien0805 <6998dragon@gmail.com>
2023-05-02 19:57:28 +0800

    fetch product data and configure nextconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.dummyjson.com",
+        port: "",
+        pathname: "/data/products/**",
+      },
+    ],
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

b888e8f0489712d4af0ed32f721cf0bb40c9b687
JustinLien0805 <6998dragon@gmail.com>
2023-05-02 22:25:17 +0800

    update readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import Product from "@/components/Product";
+import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 type Product = {
@@ -11,15 +12,19 @@ type Product = {
 };
 
 export default async function Home() {
-  const res = await fetch("https://dummyjson.com/products");
+  const res = await fetch("https://dummyjson.com/products", {
+    cache: "no-store",
+  });
   const data = await res.json();
   const products: Product[] = await data.products;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="grid grid-cols-2 gap-4">
-        {products.map((product) => (
-          <Product key={product.id} product={product} />
-        ))}
+        <Suspense fallback={<div>Loading...</div>}>
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </Suspense>
       </div>
     </main>
   );
